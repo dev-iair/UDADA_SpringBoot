4ceebf57d7ad4600a8425e880b2aa43ff9eec24c
iair <dev.iair.918@gmail.com>
2022-01-08 21:05:05 +0900

    start_migration_project

---
+++ src/test/java/ga/udada/udada/UdadaApplicationTests.java
@@ -0,0 +1,13 @@
+package ga.udada.udada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
